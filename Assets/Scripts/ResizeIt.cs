104c82abb3e32786172007bae2e1939e9ebc4929
Jacob Cazabon <j@cob.im>
2021-09-27 21:37:35 -0400

    rename scene, fix rotate script (#6)

--- Assets/Scripts/ResizeIt.cs
+++ Assets/Scripts/ResizeIt.cs
@@ -6,45 +6,47 @@ public class ResizeIt : MonoBehaviour
 {
     public float movementSpeed = 1f;
     Vector3 Size;
-    public bool x = true;
+    private bool growing = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        while(x == true) {
-            if(Size.x != 4f) {
-
-                Size = transform.localScale;
-
+        Size = transform.localScale;
+        if (growing)
+        {
+            if (Size.x <= 4f)
+            {
                 Size.x += Time.deltaTime;
                 Size.y += Time.deltaTime;
                 Size.z += Time.deltaTime;
 
                 transform.localScale = Size;
-
-            } else if(Size.x == 4f) {
-                
-                while(Size.x != 1f) {
-            
-                    Size = transform.localScale;
-
-                    Size.x -= Time.deltaTime;
-                    Size.y -= Time.deltaTime;
-                    Size.z -= Time.deltaTime;
-
-                    transform.localScale = Size;
-                }
+            }
+            else
+            {
+                growing = false;
             }
         }
+        else
+        {
+            if (Size.x >= 1f)
+            {
+                Size.x -= Time.deltaTime;
+                Size.y -= Time.deltaTime;
+                Size.z -= Time.deltaTime;
 
-        // Vector3 resize = new Vector3(Mathf.Sin(-Time.time)+1, Mathf.Sin(-Time.time)+1, Mathf.Sin(-Time.time)+1);
-        // transform.localScale = resize;
+                transform.localScale = Size;
+            }
+            else
+            {
+                growing = true;
+            }
+        }
     }
 }
